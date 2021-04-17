4772b704fc5733c53375e36fc745654e0ed1dd1e
Thomas Herring <teh6@rice.edu>
2021-04-17 16:52:41 -0500

    Added new simplified alarm

--- lib/ApolloBVM/alarm.cpp
+++ lib/ApolloBVM/alarm.cpp
@@ -2,42 +2,36 @@
 
 void AlarmManager::addAlarm(int idx, const Alarm &a) { _alarms[idx] = a; }
 
-void AlarmManager::addAlarm(int idx, AlarmFunc condition, bool blink_led,
-                            Color led_color, bool stop_running,
-                            bool sound_buzzer, int display_time,
-                            bool cancelable, String top_text,
-                            String bottom_text) {
-  _alarms[idx] =
-      Alarm(condition, blink_led, led_color, stop_running, sound_buzzer,
-            display_time, cancelable, top_text, bottom_text);
+void AlarmManager::addAlarm(int idx, AlarmFunc condition, String text) {
+  _alarms[idx] = Alarm(condition, text);
 }
 
-bool AlarmManager::evaluate() {
+Alarm &AlarmManager::evaluate() {
 
   /* int delta_time = millis() - _prev_time; */
   /* _prev_time = millis(); */
 
   for (int i = 0; i < _alarms_len; i++) {
     // Check if each alarm has been triggered.
-    if (_alarms[i].condition(_vio)) {
-      // Add the delta time to the total time this alarm has been triggered.
-      /* cur.time_triggered += delta_time; */
-      _alarms[i].time_triggered++;
-      Serial.println(_alarms[i].time_triggered);
+    if (_alarms[i].condition(_aio)) {
 
-      if (_alarms[i].time_triggered > _time_until_display) {
+      // Increment the number of cycles this alarm has been triggered.
+      _alarms[i].cycles_triggered++;
 
-        /*   // TODO: Change the Alarm Panel to display this alarm. */
-
-        return true;
+      if (_alarms[i].cycles_triggered > _time_until_display) {
+        return &_alarms[i];
       }
     } else {
-      /* // TODO: Add time that alarm hasn't been triggered for to know when to
-       */
-      /* // reset the time_triggered counter. */
+
+      // If we've seen that this alarm has been triggered many times in the
+      // past, reset.
+      /* TODO:  <17-04-21, Therring> Add logic for counting cycles where alarm
+       * is false before resetting.*/
+
+      _alarms[i].cycles_triggered = 0;
     }
   }
 
   // No alarm should be displayed.
-  return false;
+  return 0;
 }
--- lib/ApolloBVM/alarm.h
+++ lib/ApolloBVM/alarm.h
@@ -1,6 +1,6 @@
-#include "ventproperties.h"
+#include "alarmproperties.h"
 
-using AlarmFunc = bool (*)(VentIO &vio);
+using AlarmFunc = bool (*)(AlarmIO &aio);
 
 enum Color { red, orange, green };
 
@@ -12,53 +12,35 @@ public:
   ~Alarm() {}
 
   // Populating constructor.
-  Alarm(AlarmFunc condition, bool blink_led = true, Color led_color = red,
-        bool stop_running = true, bool sound_buzzer = true,
-        int display_time = -1, bool cancelable = false, String top_text = "top",
-        String bottom_text = "bottom")
-      : condition(condition), blink_led(blink_led), led_color(led_color),
-        stop_running(stop_running), sound_buzzer(sound_buzzer),
-        display_time(display_time), cancelable(cancelable), top_text(top_text),
-        bottom_text(bottom_text) {}
+  Alarm(AlarmFunc condition, String text = "alarm")
+      : condition(condition), text(text) {}
 
   // Lambda function evaluating to true when alarm is triggered.
   AlarmFunc condition;
 
-  // Alarm actions to take.
-  bool blink_led;
-  Color led_color;
-  bool stop_running;
-  bool sound_buzzer;
-
-  // Alarm display parameters.
-  int display_time;
-  bool cancelable;
-  String top_text;
-  String bottom_text;
+  String text;
 
   // Alarm state variables.
   /* bool prev_triggered; */
-  int time_triggered = 0;
+  int cycles_triggered = 0;
   /* int cycles_triggered; */
 };
 
 class AlarmManager {
 
 public:
-  AlarmManager(VentIO &vio, int num_alarms)
+  AlarmManager(AlarmIO &vio, int num_alarms)
       : _vio(vio), _alarms_len(num_alarms), _prev_time(0) {
     _alarms = new Alarm[num_alarms];
   }
 
   void addAlarm(int idx, const Alarm &a);
-  void addAlarm(int idx, AlarmFunc condition, bool blink_led, Color led_color,
-                bool stop_running, bool sound_buzzer, int display_time,
-                bool cancelable, String top_text, String bottom_text);
-  bool evaluate();
+  void addAlarm(int idx, AlarmFunc condition, String text);
+  Alarm &evaluate();
 
 private:
   // Ventilator IO reference.
-  VentIO &_vio;
+  AlarmIO &aio;
 
   // Array of alarms to evaluate.
   Alarm *_alarms;
